78db0b5910e1888f6ea0d6ce276e67ab3c8b80dc
Sergey Boyko <badwolf6661@icloud.com>
2021-12-19 15:12:30 +0300

    Add model tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 1}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
